d68451ea7c825c51517ddf4583bb475ec4a43a20
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-11 17:29:51 +0100

    FEAT: BOMB  OR ALIVE

--- js/myscript.js
+++ js/myscript.js
@@ -6,7 +6,8 @@ console.log("Ciao Cianuro... oggi si continua a giocare a CAMPO MINATO");
 const grigliaElement = document.getElementById("griglia");
 const playBtnElement = document.getElementById("play-button");
 let level = document.getElementById("livello");
-console.log(grigliaElement, playBtnElement, level);
+let gameOverElement = document.getElementById("end-game");
+console.log(grigliaElement, playBtnElement, level, gameOverElement);
 
 //********************
 // APERTURA addEventListener su BOTTONE PLAY
@@ -21,6 +22,7 @@ playBtnElement.addEventListener("click", function () {
 
   // RESETTO IL PLAY GROUND AD OGNI CLICK SUL PULSANTE PLAY
   grigliaElement.innerHTML = "";
+  gameOverElement.classList.add("d-none");
 
   // COMPONGO LA GRIGLIA DI GIOCO
   let celle = gridCellComposition(parseInt(level.value));
@@ -40,27 +42,25 @@ playBtnElement.addEventListener("click", function () {
     let lose = bombPosition(parseInt(level.value));
     console.log(lose);
     // AGGIUNGO COMPORTAMENTO AL CLICK DELLA SINGOLA CELLA
-
+    console.log(cellElement);
+    let confronto = caselle(parseInt(level.value));
+    console.log(confronto);
     cellElement.addEventListener("click", function () {
+      console.log(confronto);
       for (let i = 0; i < celle2; i++) {
-        if (index === lose[i]) {
+        if (lose.includes(confronto[i])) {
           cellElement.classList.add("bomb");
-          // grigliaElement.innerHTML = "";
-        } else {
-          cellElement.innerHTML = "+1";
+          cellElement.innerHTML = "BOMB";
+          let finish = endGame();
+        } else if (confronto[i] != lose[i]) {
+          cellElement.innerHTML = "alive";
           cellElement.classList.add("on-click");
         }
+        // } else if (confronto.lenght > 16) {
+        // }
       }
-      // console.log("Hai cliccato sulla cella " + index);
-      console.log(
-        index,
-        celle,
-        parseInt(lose),
-        // celle2 - parseInt(lose),
-        lose.lenght
-      );
-      // cellElement.classList.add("bomb");
-      // cellElement.innerHTML = "+1";
+
+      // CHIUSURA EVENT LISTENER SU CLICK CASELLE
     });
 
     // CHIUSURA CICLO FOR
@@ -128,13 +128,13 @@ function bombPosition(livello) {
 
   while (bombe.length < 16) {
     let bomb = Math.floor(Math.random() * (max - 1 + 1)) + 1;
-    console.log(bomb);
+    // console.log(bomb);
     if (bombe.includes(bomb) === false) {
       bombe.push(bomb);
     }
-    console.log(bombe.length, bombe);
+    // console.log(bombe.length, bombe);
   }
-  console.log(bombe);
+  // console.log(bombe);
   return bombe;
 
   //..........................................................
@@ -144,3 +144,39 @@ function bombPosition(livello) {
   // console.log(nomeVariabile);
   //..........................................................
 }
+//---------------------------------------------------------------------------
+// INIZIALIZZO FUNZIONE GAME OVER
+//---------------------------------------------------------------------------
+function endGame() {
+  grigliaElement.innerHTML = "";
+  playBtnElement.innerHTML = "PLAY AGAIN";
+  gameOverElement.classList.remove("d-none");
+}
+//---------------------------------------------------------------------------
+// INIZIALIZZO funzione ARRAI DI COMPARAZIONE
+//---------------------------------------------------------------------------
+function caselle(livello) {
+  let num;
+  if (livello === 1) {
+    num = 100;
+  } else if (livello === 2) {
+    num = 81;
+  } else if (livello === 3) {
+    num = 49;
+  }
+
+  let simone = [];
+
+  while (simone.length < num) {
+    let bomb = Math.floor(Math.random() * (num - 1 + 1)) + 1;
+
+    simone.push(bomb);
+  }
+  return simone;
+}
+//..........................................................
+//  COME RICHIAMARE LA FUNZIONE:
+// let nomeVariabile = gridCellComposition(parseInt(level.value));
+//  Dove in questo caso level.value sta per il numero max di composizione del range... il valore minimo è autosettato ad 1
+// console.log(nomeVariabile);
+//..........................................................
