9e69553ce78b6fbf39e0973b61119db41baf0ddc
Cianuro-83 <buzzeosimone@yahoo.com>
2023-02-11 11:55:06 +0100

    FEAT: bomb and win

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,28 @@
 console.log("Ciao Cianuro... oggi si continua a giocare a CAMPO MINATO");
-// .....................................................................................................RICHIAMO ELEMENTI DAL DOM
-// .....................................................................................................
+//********************
+// RICHIAMO ELEMENTI DAL DOM
+//******************** */
+
 const grigliaElement = document.getElementById("griglia");
 const playBtnElement = document.getElementById("play-button");
 let level = document.getElementById("livello");
 console.log(grigliaElement, playBtnElement, level);
 
-// .....................................................................................................APERTURA addEventListener su BOTTONE PLAY
-// .....................................................................................................
+//********************
+// APERTURA addEventListener su BOTTONE PLAY
+//******************** */
+
 playBtnElement.addEventListener("click", function () {
   console.log("INIZIO GIOCO");
 
-  // .....................................................................................................CICLO LE CELLE CON FOR PER CREARE LA GRIGLIA SUL DOM
-  // ..............................................................................
+  //********************
+  // CICLO LE CELLE CON FOR PER CREARE LA GRIGLIA SUL DOM
+  //******************** */
+
   // RESETTO IL PLAY GROUND AD OGNI CLICK SUL PULSANTE PLAY
   grigliaElement.innerHTML = "";
+
+  // COMPONGO LA GRIGLIA DI GIOCO
   let celle = gridCellComposition(parseInt(level.value));
   let celle2 = celle ** 2;
   console.log(celle, celle2);
@@ -28,25 +36,47 @@ playBtnElement.addEventListener("click", function () {
     cellElement.innerHTML = index;
     console.log(cellElement);
 
+    // INVOCO LA FUNZIONE DELLE BOMBE
+    let lose = bombPosition(parseInt(level.value));
+    console.log(lose);
     // AGGIUNGO COMPORTAMENTO AL CLICK DELLA SINGOLA CELLA
+
     cellElement.addEventListener("click", function () {
-      console.log("Hai cliccato sulla cella " + index);
-      cellElement.classList.add("on-click");
+      for (let i = 0; i < lose.length; i++) {
+        if (index === parseInt(lose[i])) {
+          cellElement.classList.add("bomb");
+          cellElement.innerHTML = "0";
+        } else {
+          cellElement.innerHTML = "+1";
+          cellElement.classList.add("on-click");
+        }
+      }
+      // console.log("Hai cliccato sulla cella " + index);
+      // console.log(index);
+      // cellElement.classList.add("bomb");
+      // cellElement.innerHTML = "+1";
     });
+
     // CHIUSURA CICLO FOR
   }
-  // .....................................................................................................RESETTO LA CONSOLE PER INIZIO  UOVA PARTITA
-  // .....................................................................................................
+  //********************
+  // RESETTO LA CONSOLE PER INIZIO  NUOVA PARTITA
+  //******************** */
+
   // console.clear();
-  // console.log("STAI INIZIANDO UNA NUOVA PARTITA DEL BONUS");
-  // ...................................................................................................CHIUSURA addEventListener su BOTTONE PLAY
-  // .....................................................................................................
+  // console.log("STAI INIZIANDO UNA NUOVA PARTITA");
+
+  //********************
+  // CHIUSURA addEventListener su BOTTONE PLAY
+  //********************
 });
-// .....................................................................................................
-// .....................................................................................................// .....................................................................................................CREO LE FUNZIONI
-// .....................................................................................................
-// .....................................................................................................FUNZION: GRID CELL COMPOSITIN IN BASE AI LIVELLI
-// .....................................................................................................
+//---------------------------------------------------------------------------
+// INIZIALIZZO LE FUNZIONI
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
+// FUNZIONE: GRID CELL COMPOSITIN IN BASE AI LIVELLI (TESTATA E FUNZIONANTE)
+//---------------------------------------------------------------------------
 
 function gridCellComposition(livello) {
   let gridCell;
@@ -64,36 +94,47 @@ function gridCellComposition(livello) {
     gridCell = gridSize ** 2;
     console.log(level.value);
   }
-  // let griCell = gridSize ** 2;
+
   return parseInt(gridSize);
+  //..........................................................
+  //  COME RICHIAMARE LA FUNZIONE:
+  // let nomeVariabile = bombPosition(parseInt(level.value));
+  // Dove in questo caso level.value sta per il numero di caselle che voglio su ogni riga e viene deciso da una select presente in html
+  // console.log(nomeVariabile);
+  //..........................................................
 }
-// .....................................................................................................FUNZIONE: CREO POSIZIONE BOMBE
-// .....................................................................................................
-function bombPosition(max) {
+
+//---------------------------------------------------------------------------
+// INIZIALIZZO FUNZIONE: GRID CELL COMPOSITIN IN BASE AI LIVELLI (TESTATA E FUNZIONANTE)
+//---------------------------------------------------------------------------
+
+function bombPosition(livello) {
+  let max;
+  if (livello === 1) {
+    max = 100;
+  } else if (livello === 2) {
+    max = 81;
+  } else if (livello === 3) {
+    max = 49;
+  }
+
   let bombe = [];
-  let maxb;
-  if (max === 1) {
-    maxb = 10 ** 2;
-  } else if (max === 2) {
-    maxb = 9 ** 2;
-  } else if (max === 3) {
-    maxb = 7 ** 2;
-  }
 
-  while ((bombe.length = 16)) {
-    let bomb = Math.floor(Math.random() * (parseInt(maxb) - 1 + 1)) + 1;
+  while (bombe.length < 16) {
+    let bomb = Math.floor(Math.random() * (max - 1 + 1)) + 1;
     console.log(bomb);
+    if (bombe.includes(bomb) === false) {
+      bombe.push(bomb);
+    }
+    console.log(bombe.length, bombe);
   }
-  if (bombe.includes(bomb) === false) {
-    bombe.push(bomb);
-  }
-  console.log(bombe.length, bombe);
-  return parseInt(bombe);
+  console.log(bombe);
+  return bombe;
 
-  // console.log(bombPosition());
+  //..........................................................
+  //  COME RICHIAMARE LA FUNZIONE:
+  // let nomeVariabile = gridCellComposition(parseInt(level.value));
+  //  Dove in questo caso level.value sta per il numero max di composizione del range... il valore minimo è autosettato ad 1
+  // console.log(nomeVariabile);
+  //..........................................................
 }
-// .....................................................................................................FUNZIONE: CLICK SULLA CELLA
-// .....................................................................................................
-
-let casa = bombPosition(parseInt(level.value));
-console.log(casa);
